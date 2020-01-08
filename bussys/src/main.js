5ac606ff809ec2e648389cccb18b7aef2d0ab3f6
wawoyuanli <1583649818@qq.com>
2020-01-08 18:21:24 +0800

    commit

--- bussys/src/main.js
+++ bussys/src/main.js
@@ -1,6 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Button from 'ant-design-vue/lib/button';
+import 'ant-design-vue/dist/antd.css';
 
+Vue.component(Button.name, Button);
 Vue.config.productionTip = false
 
 new Vue({
